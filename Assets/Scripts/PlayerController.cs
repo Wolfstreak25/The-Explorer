6204a581cceda2b9ad53b807848979f34e0f4485
Wolfstreak25 <108297565+Wolfstreak25@users.noreply.github.com>
2022-09-24 07:00:46 +0530

    Module 2 CH 02 mini assignment 1, 2 & assignment crouch
    as for this one I've combined all three features
    1. Added player controller script to the player which can be accessed within Assets/Scripts
    2. added animation control through script . also added flip functionality now the character can walk and run in both direction
    3. customized control for crouch and jump animation as well as I've set the custom box collider for crouch animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,22 +5,88 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
+    
+    bool crouch = false;
 
+    float offX,offY,sizeX,sizeY;
+    private BoxCollider2D boxCol;
+
+	void Start()
+    {
+        boxCol = this.GetComponent<BoxCollider2D>();
+    }
     public void Update()
     {
+        
         float speed = Input.GetAxisRaw("Horizontal");
+        if (Input.GetKey(KeyCode.LeftShift))
+            {
+                speed = speed*2 ;
+            }
         animator.SetFloat("Speed", Mathf.Abs(speed));
+        
+        float VerticalInput = Input.GetAxis("Vertical");		
 
-        Vector3 scale = transform.localScale;
+		    JumpAnimation(VerticalInput);
+   
+			if (Input.GetKey(KeyCode.LeftControl) && crouch == false)
+            {
+                Crouch(true);
+                crouch = true;
+            }
+            else if (Input.GetKey(KeyCode.LeftControl) && crouch == true)
+            {
+                Crouch(false);
+                crouch = false;
+            }
 
-        if(speed < 0)
+            if (Input.GetKey(KeyCode.E))
+            {
+                animator.SetTrigger("Attack");
+            }
+
+            Vector3 scale = transform.localScale;
+
+            if(speed < 0)
+            {
+                scale.x = -1f * Mathf.Abs(scale.x);
+            }
+            else if(speed > 0)
+            {
+                scale.x = Mathf.Abs(scale.x);
+            }
+            transform.localScale = scale;
+            
+    }
+     public void Crouch(bool crouch)
+    {
+        if (crouch == true)
         {
-            scale.x = -1f * Mathf.Abs(scale.x);
+            offX = -0.1284856f;               //Offset X
+            offY = 0.5952377f;                //Offset Y
+
+            sizeX = 0.8512849f;               //Size X
+            sizeY = 1.315794f;               //Size Y
         }
-        else if(speed > 0)
+
+        else
         {
-            scale.x = Mathf.Abs(scale.x);
+            offX = -0.002953231f;             //Offset X
+            offY = 0.9699736f;               //Offset Y
+
+            sizeX = 0.5884072f;              //Size X
+            sizeY = 2.065266f;              //Size Y
         }
-        transform.localScale = scale;
+        animator.SetBool("Crouch", crouch);
+
+        boxCol.size = new Vector2(sizeX, sizeY);
+        boxCol.offset = new Vector2(offX, offY);
+    }
+    public void JumpAnimation(float vertical)
+    {    
+		if (vertical > 0 )
+        {
+            animator.SetTrigger("Jump");            
+        }
     }
 }
